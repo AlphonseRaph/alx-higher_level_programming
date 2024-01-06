298551389bee908a69a26ac07435d8965f42351d
AlphonseRaph <alphonseoketch2004@gmail.com>
2024-01-06 06:06:40 -0800

    Indexing string

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+word = "Holberton"
+length = len(word)
+lenghttouse = length - 1
+print(f"First 3 letters: {word[0:3]}")
+print(f"Last 2 letters: {word[-2:]}")
+print(f"Middle word: {word[1:lenghttouse]}")
