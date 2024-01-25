52526b3c9b2088ddcfe39fabc24b3a352e5ed943
AlphonseRaph <alphonseoketch2004@gmail.com>
2024-01-25 04:03:22 -0800

    Last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,10 @@ no = abs(number) % 10
 if number < 0:
     no = -no
 if no > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, no))
+    print(("Last digit of {:d} is {:d} and is greater than 5"
+           .format(number, no)))
 elif no == 0:
     print("Last digit of {:d} is {:d} and is 0".format(number, no))
 elif no < 6 and no != 0:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, no))
+    print(("Last digit of {:d} is {:d} and is less than 6 and not 0"
+           .format(number, no)))
